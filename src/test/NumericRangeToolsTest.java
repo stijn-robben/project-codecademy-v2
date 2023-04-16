a335ed3ae8c6fb72483fe9e422487e4ac7f89375
Bas09 <74872950+Bas09@users.noreply.github.com>
2023-04-16 21:21:22 +0200

    PostalCode + NumericRangetool test added

--- src/test/NumericRangeToolsTest.java
+++ src/test/NumericRangeToolsTest.java
@@ -1,5 +1,11 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import InputVerification.NumericRangeTool;
+
 public class NumericRangeToolsTest {
 
     /**
@@ -9,21 +15,73 @@ public class NumericRangeToolsTest {
      *   @requires 0 <= percentage <= 100;
      *   @ensures \result = true;
      * }
-     * 
-     * @subcontract value out of range low {
+     */
+
+     @Test
+     public void testInputIsValidRangeRequires0EnsuresTrue(){
+         //Arrange
+         int percentage = 0;
+         //Act
+         Boolean result = NumericRangeTool.validPercentage(percentage);
+         //Assert
+         assertEquals(true, result);
+     }
+ 
+     @Test
+     public void testInputIsValidRangeRequires50EnsuresTrue(){
+         //Arrange
+         int percentage = 50;
+         //Act
+         Boolean result = NumericRangeTool.validPercentage(percentage);
+         //Assert
+         assertEquals(true, result);
+     }
+ 
+ 
+     @Test
+     public void testInputIsValidRangeRequires100EnsuresTrue(){
+         //Arrange
+         int percentage = 100;
+         //Act
+         Boolean result = NumericRangeTool.validPercentage(percentage);
+         //Assert
+         assertEquals(true, result);
+     }
+
+
+     /* @subcontract value out of range low {
      *   @requires percentage < 0;
      *   @ensures \result = false;
      * }
-     * 
-     * @subcontract value out of range high {
+     */
+
+     @Test
+     public void testInputIsValidRangeRequiresMin1EnsuresFalse(){
+         //Arrange
+         int percentage = -1;
+         //Act
+         Boolean result = NumericRangeTool.validPercentage(percentage);
+         //Assert
+         assertEquals(false, result);
+     }
+
+
+     /* @subcontract value out of range high {
      *   @requires percentage > 100;
      *   @signals \result = false;
      * }
      * 
      */
-    public static boolean isValidPercentage(int percentage) {
 
-        return false;
-    }    
+     @Test
+     public void testInputIsValidRangeRequires101EnsuresFalse(){
+         //Arrange
+         int percentage = 101;
+         //Act
+         Boolean result = NumericRangeTool.validPercentage(percentage);
+         //Assert
+         assertEquals(false, result);
+     }
+     
 }
 
